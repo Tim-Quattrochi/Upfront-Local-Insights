2da7473e80e5572a12f5c31d0aaf3db818b37988
Tim Quattrochi <timq82@gmail.com>
2023-03-07 23:45:27 -0500

    landing, register form

---
+++ packages/client/src/pages/Landing.jsx
@@ -0,0 +1,11 @@
+import landingImage from "../assets/landingImage.svg";
+
+const Landing = () => {
+  return (
+    <div>
+      <img src={landingImage} alt="" />
+    </div>
+  );
+};
+
+export default Landing;
--- packages/client/tailwind.config.cjs
+++ packages/client/tailwind.config.cjs
@@ -4,5 +4,30 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#09AF7B",
+
+          secondary: "#6bbbe0",
+
+          accent: "#ffccea",
+
+          neutral: "#1D1A23",
+
+          "base-100": "#167EBF",
+
+          info: "#09AF7B",
+
+          success: "#118866",
+
+          warning: "#EC8609",
+
+          error: "#E95744",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
 };
