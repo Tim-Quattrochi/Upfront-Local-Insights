7b5793684bb96b25f3c40eb9937c4a2daebe580a
Tim Quattrochi <timq82@gmail.com>
2023-03-21 23:24:24 -0400

    added morgan middleware for server logs.

--- packages/server/server.js
+++ packages/server/server.js
@@ -3,6 +3,7 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const path = require("path");
+const morgan = require("morgan");
 
 const connectMyDB = require("./config/db");
 const { login } = require("./controllers/userController");
@@ -20,6 +21,7 @@ app.use(
   })
 );
 
+app.use(morgan("tiny"));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: true }));
