cb2e786c11eaf0ea654fdf5fabde78ece673d0a9
Tim Quattrochi <timq82@gmail.com>
2023-10-28 12:11:37 -0400

    new landing page styling

--- packages/client/src/components/Landing.jsx
+++ packages/client/src/components/Landing.jsx
@@ -13,41 +13,24 @@ const Landing = () => {
   }, []);
 
   return (
-    <>
-      <div className=" w-full flex flex-wrap justify-center content-center mx-auto min-h-screen ">
-        <div className="max-w-md mb-20">
-          <h1 className=" text-2xl font-bold relative text-center text-cyan-700 border border-1  border-cyan-500 shadow-md rounded ">
-            Real and{" "}
-            <span className="italic underline decoration-pink-500">
-              unbiased
-            </span>{" "}
-            Business reviews
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content text-center">
+        <div className="max-w-md">
+          <h1 className="text-4xl font-bold">
+            Real consumer reviews
           </h1>
-
-          <div className="py-6 mt-5 text-xl">
-            We want your reviews to be seen, good or bad.
-          </div>
-          <div className="py-6 text-center ">
-            Our goal is{" "}
-            <span className="font-bold underline  decoration-pink-500">
-              simple.
-            </span>
-            <p className="bg-white shadow-md w-auto mt-2">
-              We believe all consumer reviews should be seen. Google
-              and Yelp! both hide reviews. We aim to provide full
-              transparency.
-            </p>
-          </div>
-          <div className="mx-auto">
-            <Link to={"/register"}>
-              <button className="btn btn-primary mx-auto ">
-                Get Started
-              </button>
-            </Link>
-          </div>
+          <p className="py-6">
+            We want your reviews to be seen, good or bad. Plenty of
+            review sites are around, but we want to be different. We
+            want consumers to be able to see the good and the bad, so
+            they can make an informed decision.
+          </p>
+          <Link to={"/register"}>
+            <button className="btn btn-primary">Get Started</button>
+          </Link>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
