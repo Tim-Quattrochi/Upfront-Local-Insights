d3bb40aa6449194ddce9d92564309d6159cd56c4
Tim Quattrochi <timq82@gmail.com>
2024-07-17 01:43:56 -0400

    refactor: Update Avatar and Navbar components

--- packages/client/src/components/Avatar.jsx
+++ packages/client/src/components/Avatar.jsx
@@ -2,11 +2,27 @@ import React from "react";
 
 export const Avatar = ({ name }) => {
   return (
-    <div className="avatar placeholder">
-      <div className="bg-neutral text-neutral-content w-8 md:w-12 rounded-full p-2 m-2">
-        <span className="text-xs md:text-md lg:text-lg">
+    <div className="m-2 px-4">
+      <div className="relative inline-flex items-center justify-center w-14 h-14 md:w-20 md:h-20 lg:w-20 lg:h-20 rounded-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 border-4 border-white shadow-lg hover:shadow-2xl transition-shadow duration-300 ease-in-out">
+        <span className="text-sm md:text-2xl lg:text-xl text-white font-semibold">
           {name.toUpperCase()}
         </span>
+        <div className="absolute -bottom-1 -right-1 bg-white rounded-full p-1">
+          <svg
+            className="w-4 h-3 text-indigo-500"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M5 13l4 4L19 7"
+            ></path>
+          </svg>
+        </div>
       </div>
     </div>
   );
--- packages/client/src/components/Navbar.jsx
+++ packages/client/src/components/Navbar.jsx
@@ -107,26 +107,36 @@ export const Navbar = () => {
   }, []);
 
   return (
-    <div className="flex justify-around  h-24 bg-primary">
-      <div className="m-5 p-1 mr-auto">
-        <Link
-          to="/"
-          className="btn btn-ghost normal-case md:text-3xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-accent to-secondary hover:from-secondary hover:to-accent transition-all duration-300 ease-in-out shadow-sm hover:shadow-md tracking-wide"
-        >
-          Upfront Local Insights
+    <div className="flex justify-between h-28 bg-primary">
+      <div className="mr-auto">
+        <Link to="/" className="mr">
+          <img
+            src="/logo.svg"
+            alt="logo"
+            className="h-28 object-cover"
+          />
         </Link>
       </div>
 
-      <div className="flex justify-center items-center flex-wrap">
+      <div className="flex justify-center items-baseline flex-wrap">
         <div className="form-control relative">
           <input
             type="text"
             disabled={loading || isLongLoad}
             placeholder="Search a business"
-            className="inpu bg-gray-200 input-bordered w-20 h-auto md:w-auto m-5 p-1 placeholder:text-black placeholder:p-4 placeholder:italic"
+            className="input hidden md:block bg-gray-200 input-bordered w-20 h-auto md:w-auto m-5 p-1 placeholder:text-black placeholder:p-4 placeholder:italic"
             value={navbarSearchTerm}
             onChange={handleNavBarSearch}
           />
+          <div className="min-h-12 relative">
+            {shouldDisplayMessage && (
+              <div className="mx-auto">
+                <p className="relative font-bold text-center">
+                  {message}
+                </p>
+              </div>
+            )}
+          </div>
           {searchFilter.length > 0 ? (
             <div
               className="absolute z-10 mt-20  bg-white w-64"
@@ -161,11 +171,6 @@ export const Navbar = () => {
           ) : (
             ""
           )}
-          {shouldDisplayMessage && (
-            <div className=" font-bold ">
-              <p className="px-2 pb-6">{message}</p>
-            </div>
-          )}
         </div>
         <div className="dropdown dropdown-end">
           <label tabIndex={0}>
@@ -175,7 +180,7 @@ export const Navbar = () => {
           </label>
           <ul
             tabIndex={0}
-            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+            className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-blue-100 rounded-box w-45"
           >
             <li>
               <Link
