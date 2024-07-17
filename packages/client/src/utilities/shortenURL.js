8147df382760e071d9acd9c6a41fe3d52b6f8d8a
Tim Quattrochi <timq82@gmail.com>
2024-07-17 07:25:09 -0400

    test: test and optimize helper

--- packages/client/src/utilities/shortenURL.js
+++ packages/client/src/utilities/shortenURL.js
@@ -1,5 +1,12 @@
+const isValidURL =
+  /^(http(s)?:\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.(com|org|net|anotherTLD)\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+
 export const truncateAfterDomain = (url) => {
-  if (!url) return "";
+  if (!url || typeof url !== "string") return "";
+  if (!isValidURL.test(url)) {
+    throw new Error("Invalid URL");
+  }
+
   const urlObj = new URL(url);
   return urlObj.hostname;
 };
